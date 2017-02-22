20301a5517215d9da5a2ce138560048da28c10ec
unknown <tfs1@S0106WEB1T.IGWEB.LOCAL>
2017-02-22 07:51:23 -0500

    SB build 16.2.20162.282

--- JA/HtmlSamples/combo/filtering/demo.js
+++ JA/HtmlSamples/combo/filtering/demo.js
@@ -6,9 +6,8 @@ $(function () {
                 valueKey: "ProductID",
                 dataSource: northwindProducts,
                 filteringType: "local",
-                highlightMatchesMode: "startsWith",
-                filteringCondition: "startsWith",
-                autoComplete: true
+                autoComplete: true,
+                placeHolder: "Focus me."
             });
 
             $("#filterContainsCombo").igCombo({
@@ -18,7 +17,8 @@ $(function () {
                 dataSource: northwindProducts,
                 filteringType: "local",
                 filteringCondition: "contains",
-                highlightMatchesMode: "contains"
+                highlightMatchesMode: "contains",
+                placeHolder: "Focus me."
             });
 
             $("#caseSensitiveCombo").igCombo({
@@ -29,9 +29,46 @@ $(function () {
                 filteringType: "local",
                 autoSelectFirstMatch: true,
                 caseSensitive: true,
-                autoComplete: true
+                autoComplete: true,
+                placeHolder: "Focus me."
             });
 
+
+            $("#autoCompleteCombo").igNotifier({
+                showOn: "focus",
+                state: "success",
+                direction: "auto",
+                closeOnBlur: true,
+                showIcon: false,
+                mode: "popover",
+                messages: {
+                    success: "StartsWith フィルター条件およびオートコンプリート"
+                }
+            });
+
+            $("#filterContainsCombo").igNotifier({
+                showOn: "focus",
+                state: "success",
+                direction: "auto",
+                showIcon: false,
+                closeOnBlur: true,
+                mode: "popover",
+                messages: {
+                    success: "Contains フィルター条件"
+                }
+            });
+
+            $("#caseSensitiveCombo").igNotifier({
+                showOn: "focus",
+                state: "success",
+                direction: "auto",
+                showIcon: false,
+                closeOnBlur: true,
+                mode: "popover",
+                messages: {
+                    success: "大文字と小文字の区別"
+                }
+            });
         });
 
      
